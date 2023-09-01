61e0152c6039f33b59e91c5dfaff0119fd7474f2
kosme <casadadaista@gmail>
2023-09-01 21:33:42 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	// import { COLOR } from './../../.svelte-kit/ambient.d.ts';
+	// import '../app.postcss';
+	// import './styles.css';
+	// import '../scss/style.css';
+	// import Footer from './components/Footer.svelte';
+	// import List from './components/List.svelte';
+</script>
+
+<div class="app">
+
+<!-- <List/> -->
+	<main>
+		<slot />
+	</main>
+
+	
+	<!-- <Footer>
+		<slot />
+	</Footer> -->
+</div>
+
+<style>
+a{
+	color: inherit;
+
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../scss/style.css';
+</script>
+
+<section>
+    <h1>hi</h1>
+    <h2>his</h2>
+    <h3>it's so seething</h3>
+</section>
